d361904b2e0cdbaa25cba2a609e25fb4168ca541
sinceseptember4 <sinceseptember4@gmail.com>
2022-12-29 22:21:14 +0900

    second

---
+++ src/component/home.tsx
@@ -0,0 +1,21 @@
+import Button from 'react-bootstrap/Button';
+const Home = () => {
+    
+    return (
+        <>
+        <h1>test</h1>
+        <Button variant="outline-secondary">編集</Button>
+        <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+        </>
+    );
+};
+
+export default Home;
